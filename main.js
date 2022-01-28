17e05662b1aaaab7fc3412b53b63eeaf8bd092d4
SmashingPrograms <smashingprograms@gmail.com>
2022-01-28 10:39:40 -0500

    Strictly equal to

--- main.js
+++ main.js
@@ -62,20 +62,20 @@ class AttEnemy extends Character{
 }
 
 const attDamage = function(){
-    let attVar = 0
+    let attVar = 0;
     return attVar + (Math.floor(Math.random * 5) + 1);
 }
 
 const defDamage = function(){
     let defVar = 0;
-    if(player == DefEnemy || player == DefPlayer){
+    if(player === DefEnemy || player === DefPlayer){
         return defVar + (Math.floor(Math.random * 7) + 1);
-    } else if (player == MedEnemy || player == MedPlayer){
+    } else if (player === MedEnemy || player === MedPlayer){
         return defVar + (Math.floor(Math.random * 6) + 1);
     } else {
         return defVar + (Math.floor(Math.random * 5) + 1);
-    }
-}
+    };
+};
 
 const compare = function(){
     if(defVar >= 5){
@@ -83,5 +83,5 @@ const compare = function(){
     } else if (combatAtt > combatDef){
         console.log('HIT');
         defHealth = defHealth - (combatAtt - combatDef);
-    }
-}
+    };
+};
